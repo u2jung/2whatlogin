41c57dfabcc686f44eafcd472e9f1b6207e4a751
u2jung <163082404+u2jung@users.noreply.github.com>
2024-05-24 13:26:52 +0900

    Initial Commit

---
+++ 2what/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2what
+//
+//  Created by 김유정 on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2what/_whatApp.swift
@@ -0,0 +1,17 @@
+//
+//  _whatApp.swift
+//  2what
+//
+//  Created by 김유정 on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct _whatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
